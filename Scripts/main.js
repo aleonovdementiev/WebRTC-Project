96f2cf2ce602671de1bef70623637354497c5c59
ArtemLeonov <artilien@gmail.com>
2015-11-25 00:28:41 +0200

    Small changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -16,8 +16,7 @@ var constraints = window.constraints = {
 };
 var errorElement = document.querySelector('#errorMsg');
 
-navigator.mediaDevices.getUserMedia(constraints)
-.then(function(stream) {
+navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
   var videoTracks = stream.getVideoTracks();
   console.log('Got stream with constraints:', constraints);
   console.log('Using video device: ' + videoTracks[0].label);
